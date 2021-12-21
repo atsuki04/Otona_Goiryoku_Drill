b663d0ca7165eab8c0fd1edaf8282b5ce6ba33cc
atsuki04 <cavu04hmp@gmail.com>
2021-12-21 09:27:56 +0000

    [Fix] お問合せ機能の誤字

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,7 +1,7 @@
 class InquiryMailer < ApplicationMailer
   default from: "otonanogoiryokudrill@gmail.com"  #送信元のアドレス
   
-  def recieved_email(inquiry)
+  def received_email(inquiry)
     @inquiry = inquiry
     mail(:to => inquiry.email, :subject => "お問合せを承りました")
   end  
