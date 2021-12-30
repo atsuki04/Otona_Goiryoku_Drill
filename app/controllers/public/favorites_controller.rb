31e490088b9ca9c537d7d41289721e18364cb287
atsuki04 <cavu04hmp@gmail.com>
2021-12-30 09:57:22 +0000

    いいね機能　非同期通信化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,14 +4,12 @@ class Public::FavoritesController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     @favorite = current_customer.favorites.create(question_id: @question.id)
-    redirect_to question_answer_path(@question)
   end
 
   def destroy
     @question = Question.find(params[:question_id])
     @favorite = current_customer.favorites.find_by(question_id: @question.id)
     @favorite.destroy
-    redirect_to question_answer_path(@question)
   end
 
   def my_favorites
