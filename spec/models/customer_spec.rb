7587d904522b20d43e8d17195af6a468a3d3591e
atsuki04 <cavu04hmp@gmail.com>
2022-01-30 10:37:06 +0000

    [Fix] model_test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe 'Customerモデルのテスト', type::model do
   describe 'バリデーションのテスト' do
     subject { customer.valid? }
     
-    let!(:other_user) { 
+    let!(:other_customer) { create(:customer) }
+    let(:customer) { build(:customer) }
+    
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        customer.name = ''
+        is_expected.to eq false
+      end 
+      
   end  
 end
