11bf9e32575dd44062a94c856996ebd8d77f96ec
atsuki04 <cavu04hmp@gmail.com>
2022-01-31 11:17:40 +0000

    [Add] system_test

---
+++ spec/systems/before_login_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+ describe 'ユーザ新規登録のテスト' do
+    before do
+      visit new_customer_registration_path
+    end
+    
+    context '新規登録成功のテスト' do
+      
+      
+  end    
